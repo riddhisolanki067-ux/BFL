dd470c3238cde54e49981fcbb4f00ceb96252b94
siddharthfumakiya18 <sidfumakiya18@gmail.com>
2025-12-10 00:34:33 +0530

    session default button add

--- bfl_custom/public/js/bfl_desk.js
+++ bfl_custom/public/js/bfl_desk.js
@@ -1,22 +1,50 @@
 $(document).ready(function () {
-  if (!sessionStorage.getItem("session_default_dialog_shown")) {
-    frappe.ui.toolbar.setup_session_defaults();
-    sessionStorage.setItem("session_default_dialog_shown", "1");
-  }
 
-  changeNavbarStyle();
+    console.log("called");
 
-  const newButton = $('<button/>').text("Select Company");
-  newButton.on("click", function () {
-    frappe.ui.toolbar.setup_session_defaults();
-  });
+    var newButton = $('<button id="session-company-btn"/>');
+
+    // 1Get session default company
+    let currentCompany = frappe.defaults.get_default("company");
+
+    // Default text
+    if (currentCompany) {
+        newButton.text(currentCompany);
+    } else {
+        newButton.text("Select Company");
+    }
+
+    // Style
+    newButton.css({
+        borderRadius: '5px',
+        color: 'white',
+        backgroundColor: '#0074d9',
+        border: '0px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        cursor: 'pointer',
+        padding: '6px 12px',
+    });
+
+    //  Only open popup on button click
+    newButton.on("click", function () {
+        frappe.ui.toolbar.setup_session_defaults();
+    });
+
+    // Add button
+    $('.input-group.search-bar.text-muted').before(newButton);
+
+    // Update text AFTER popup save
+    frappe.realtime.on("session_default_changed", function () {
+
+        let updatedCompany = frappe.defaults.get_default("company");
+
+        if (updatedCompany) {
+            newButton.text(updatedCompany);
+        } else {
+            newButton.text("Select Company");
+        }
+    });
+
 });
-const original_logout = frappe.app.logout;
-
-frappe.app.logout = function () {
-  // clear your flag so next login runs the dialog
-  sessionStorage.removeItem("session_default_dialog_shown");
-
-  // call original logout
-  return original_logout.apply(this, arguments);
-};
